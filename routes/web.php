abd2c1b308cf3cc11a136c6fcc9ca6ae3ed2f628
DESKTOP-O242LPO\Kyla <kylajanepikia@hotmail.com>
2017-09-28 17:06:43 +1300

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Content pages
+Route::get('/','ContentController@home');
+Route::get('/home','ContentController@home');
+Route::get('/about','ContentController@about');
+Route::get('/gallery','ContentController@gallery');
+Route::get('/news','ContentController@news');
+
+//Clients pages
+Route::get('/contact','ClientController@contact');
+Route::get('/classes/kid','ClientController@kidclass');
+Route::get('/classes/adult','ClientController@kidclass');
