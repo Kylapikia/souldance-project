72f307d3b1715ea390e5253a5e5af865722763b6
DESKTOP-O242LPO\Kyla <kylajanepikia@hotmail.com>
2017-09-28 16:45:05 +1300

    - By Kyla

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,6 @@
 |
 */
 
-//Content pages
-Route::get('/','ContentController@home');
-Route::get('/home','ContentController@home');
-Route::get('/about','ContentController@about');
-Route::get('/gallery','ContentController@gallery');
-Route::get('/news','ContentController@news');
-
-//Clients pages
-Route::get('/contact','ClientController@contact');
-Route::get('/classes/kid','ClientController@kidclass');
-Route::get('/classes/adult','ClientController@kidclass');
+Route::get('/', function () {
+    return view('welcome');
+});
